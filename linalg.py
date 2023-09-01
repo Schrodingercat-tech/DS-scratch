01fa10184ec37016f18f6b3c6a31c79cf16a6f1b
Sai Ganesh Reddy <112499676+Schrodingercat-tech@users.noreply.github.com>
2023-09-01 10:50:18 +0530

    Update linalg.py

--- linalg.py
+++ linalg.py
@@ -1,46 +1,56 @@
 import numpy as np
-from collections import defaultdict,Counter
 from typing import List
 
 vector = List[float]
 
-def add(a:vector,b:vector)->vector:
-    assert len(a)== len(b),"two vectors must have same dimension"
-    return np.array(a)+np.array(b)
+class linalg:
+    @staticmethod
+    def add(a:vector,b:vector)->vector:
+        assert len(a)== len(b),"two vectors must have same dimension"
+        return np.array(a)+np.array(b)
+    
+    @staticmethod
+    def subtract(a:vector,b:vector)->vector:
+        assert len(a)==len(b),"two vectors must have same dimension"
+        return np.array(a)-np.array(b)
 
-def subtract(a:vector,b:vector)->vector:
-    assert len(a)==len(b),"two vectors must have same dimension"
-    return np.array(a)-np.array(b)
+    @staticmethod
+    def vector_sum(vectors : List[vector])->vector:
+        assert vectors,' no vectors provided'
+        dimension = len(vectors[0])
+        assert all(len(v)==dimension for v in vectors),' All vectors must have the same dimension'
+        new_vector = np.zeros(dimension)
+        for vec in vectors:
+            new_vector+=np.array(vec)
+        return new_vector
 
-def vector_sum(vectors : List[vector])->vector:
-    assert vectors,'no vectors provided'
-    dimension = len(vectors[0])
-    assert all(len(v)==dimension for v in vectors),'All vectors must have same dimension'
-    new_vector = np.zeros(dimension)
-    for vec in vectors:
-        new_vector+=np.array(vec)
-    return new_vector
+    @staticmethod
+    def scalar_multiply(c:float,a:vector)->vector:
+        return c*np.array(a)
 
-def scalar_multiply(c:float,a:vector)->vector:
-    return c*np.array(a)
-
-def vector_mean(vectors:List[vector])->vector:
-    dimension = len(vectors)
-    return scalar_multiply(1/dimension,vector_sum(vectors))
-
-def dot (v:vector,w:vector)->float:
-    assert len(v)==len(w),'both vectors must be of same dimension'
-    return np.dot(v,w)
-
-def sum_of_squares(a:vector)->float:
-    return np.sum(np.array(a)**2)
-
-def magnitude(a:vector)->float:
-    return np.sqrt(sum_of_squares(a))
-
-def squared_distance(v:vector,w:vector)->float:
-    return sum_of_squares(subtract(v,w))
-
-def distance(v:vector,w:vector)->float:
-    return magnitude(subtract(v,w))
+    @staticmethod
+    def vector_mean(vectors:List[vector])->vector:
+        dimension = len(vectors)
+        return linalg.scalar_multiply(1/dimension,linalg.vector_sum(vectors))
+   
+    @staticmethod
+    def dot (v:vector,w:vector)->float:
+        assert len(v)==len(w),'both vectors must be of same dimension'
+        return np.dot(v,w)
+  
+    @staticmethod
+    def sum_of_squares(a:vector)->float:
+        return np.sum(np.array(a)**2)
+  
+    @staticmethod
+    def magnitude(a:vector)->float:
+        return np.sqrt(linalg.sum_of_squares(a))
+ 
+    @staticmethod
+    def squared_distance(v:vector,w:vector)->float:
+        return linalg.sum_of_squares(linalg.subtract(v,w))
+  
+    @staticmethod
+    def distance(v:vector,w:vector)->float:
+        return linalg.magnitude(linalg.subtract(v,w))
 
