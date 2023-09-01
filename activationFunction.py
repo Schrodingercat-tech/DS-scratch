d1e912c6072a5fa5493101ea545d309a302356c1
Sai Ganesh Reddy <112499676+Schrodingercat-tech@users.noreply.github.com>
2023-09-01 06:58:53 +0530

    Update activationFunction.py

--- activationFunction.py
+++ activationFunction.py
@@ -43,54 +43,54 @@ class activate:
         return x
 
     @staticmethod
-    def gaussian(x, mu=0, sigma=1):
+    def gaussian(x:float, mu=0, sigma=1):
         return np.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
 
     @staticmethod
-    def bent_identity(x):
+    def bent_identity(x:float):
         return ((np.sqrt(x ** 2 + 1) - 1) / 2) + x
 
     @staticmethod
-    def softplus(x):
+    def softplus(x:float):
         return np.log(1 + np.exp(x))
 
     @staticmethod
-    def arctan(x):
+    def arctan(x:float):
         return np.arctan(x)
 
     @staticmethod
-    def isrlu(x, alpha=1.0):
+    def isrlu(x:float, alpha=1.0):
         return np.where(x > 0, x, x / np.sqrt(1 + alpha * x ** 2))
 
     @staticmethod
-    def softsign(x):
+    def softsign(x:float):
         return x / (1 + np.abs(x))
 
     @staticmethod
-    def custom_activation(x):
+    def custom_activation(x:float):
         return np.sin(x) / x
 
     @staticmethod
-    def sqnl(x):
+    def sqnl(x:float):
         def piecewise(x):
             return np.where(x > 2.0, 1.0, x - x ** 2 / 4)
 
         return np.where(x >= 0, piecewise(x), -piecewise(-x))
 
     @staticmethod
-    def mish(x):
+    def mish(x:float):
         return x * np.tanh(np.log(1 + np.exp(x)))
 
     @staticmethod
-    def bent_exp(x):
+    def bent_exp(x:float):
         return ((np.sqrt(x ** 2 + 1) - 1) / 2) + x + np.exp(-x) - 1
 
     @staticmethod
-    def selu(x, alpha=1.67326, scale=1.0507):
+    def selu(x:float, alpha=1.67326, scale=1.0507):
         return scale * np.where(x > 0, x, alpha * (np.exp(x) - 1))
 
     @staticmethod
-    def soft_exponential(x, alpha=1.0):
+    def soft_exponential(x:float, alpha=1.0):
         return np.where(x > 0, alpha * (np.exp(x) - 1), -alpha * np.exp(-x) + 1)
 
 
