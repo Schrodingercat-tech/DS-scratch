9a9fdfb3763f81c3de735f116b5b3976b55f0f9a
Sai Ganesh Reddy <112499676+Schrodingercat-tech@users.noreply.github.com>
2023-09-01 11:02:30 +0530

    Update GradientDescent.py

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,20 +1,22 @@
-from linalg import vector,add,scalar_multiply
-from typing import Callable,TypeVar,List,Iterator
+from linalg import linalg,vector
+from typing import Callable, TypeVar, List, Iterator
 import random
+import numpy as np
 
+T = TypeVar('T') # for writing 'generic' functions
 
 # derivates
 
 def difference_quotient(
         f:Callable[[float],float],
-        x:float,
+        x: float,
         h:float=0.0001)->float:
     return (f(x+h)-f(x))/h
 
 def partial_difference_quotient(
         f:Callable[[vector],float],
-        v:vector,
-        i:int,
+        v: vector,
+        I: int,
         h:float = 0.0001)->float:
     w = [vj+(h if j==i else 0)
          for j,vj in enumerate(v)]
@@ -22,25 +24,25 @@ def partial_difference_quotient(
 
 def estimate_gradient(
         f:Callable[[vector],float],
-        v:vector,
+        v: vector,
         h:float = 0.0001):
     return [partial_difference_quotient(f,v,i,h)
             for i in range(len(v))]
 
-# using Gradient step
+# Using gradient step
 
 def gradient_step(
-        v:vector,
-        gradient:vector,
-        step_size:float):
+        v: vector,
+        gradient: vector,
+        step_size: float):
     assert len(v)==len(gradient),'dim must be same'
-    step = scalar_multiply(step_size,gradient)
-    return add(v,step)
+    step = linalg.scalar_multiply(step_size,gradient)
+    return linalg.add(v,step)
 
 def sum_of_squares_gradient(v:vector):
     return [2*vi for vi in v]
 
-# use gradient decent to fit the model
+# Use gradient decent to fit the model
 
 def linear_gradient(x:float,y:float,theta:vector)->vector:
     slope,intercept = theta
@@ -50,15 +52,12 @@ def linear_gradient(x:float,y:float,theta:vector)->vector:
     return grad
 
 # minibatch 
-
-T = TypeVar('T') # for writing 'generic' functions
-
 def mminibatches(
         dataset:List[T],
-        batch_size:int,
+        batch_size: int,
         shuffle:bool=True)->Iterator[List[T]]:
     batch_starts = [start  for start in range(0,len(dataset),batch_size)]
-    if shuffle : random.shuffle(batch_starts)
+    if shuffle: random.shuffle(batch_starts)
     for start in batch_starts:
         end = start+batch_size
         yield dataset[start:end]
